dc928f800be5f2050e7ba033c0a9a561e5ba88a3
al <alex@getquotient.ai>
2025-11-17 20:47:47 -0500

    ci: fmt

--- crates/app/src/daemon/http_server/html/mod.rs
+++ crates/app/src/daemon/http_server/html/mod.rs
@@ -21,8 +21,14 @@ pub fn router(state: ServiceState) -> Router<ServiceState> {
         .route("/", get(index::handler))
         .route("/buckets", get(index::handler))
         .route("/buckets/:bucket_id", get(buckets::file_explorer::handler))
-        .route("/buckets/:bucket_id/view", get(buckets::file_viewer::handler))
-        .route("/buckets/:bucket_id/edit", get(buckets::file_editor::handler))
+        .route(
+            "/buckets/:bucket_id/view",
+            get(buckets::file_viewer::handler),
+        )
+        .route(
+            "/buckets/:bucket_id/edit",
+            get(buckets::file_editor::handler),
+        )
         .route("/buckets/:bucket_id/logs", get(buckets::history::handler))
         .route("/buckets/:bucket_id/peers", get(buckets::peers::handler))
         .route("/gw/:bucket_id/*file_path", get(gateway::handler))
